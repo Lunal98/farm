54be79387f751c3827b0625c14535a8852451080
lunatk <lunal.pxy@gmail.com>
2021-11-07 20:03:43 +0100

    init-3

--- farm.lua
+++ farm.lua
@@ -1,1 +1,19 @@
-print "test2"
+
+print "test3"
+
+local exists, data=turtle.inspectDown()
+local state = data.metadata
+local function replant()
+    if state==3  then
+        turtle.digdown()
+        local crop=string.sub(data.name,string.len(data.name)-4)
+        local i = turtle.getItemDetail(2).name
+        local item=string.sub(i,string.len(i)-4)
+        if crop==item then
+            print ("plant")
+            turtle.placeDown(2)
+            else
+                print("Don't plant")
+        end
+    end
+end
