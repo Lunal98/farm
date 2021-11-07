ac2d0ea4e027beeb9661e49dda254a5561e5b2a3
lunatk <lunal.pxy@gmail.com>
2021-11-07 20:20:30 +0100

    init-3 debug

--- farm.lua
+++ farm.lua
@@ -1,12 +1,12 @@
 
-print "test3.1"
+print "test3.2"
 
 local exists, data=turtle.inspectDown()
 local state = data.metadata
 local function replant()
     if state==3  then
-        turtle.digdown()
         local crop=string.sub(data.name,string.len(data.name)-4)
+        turtle.digDown()
         local i = turtle.getItemDetail(2).name
         local item=string.sub(i,string.len(i)-4)
         if crop==item then
